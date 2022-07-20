89002ccdcf65524abdb4370ffee2e6fa0225b6f4
PchelaTF <PchelaTF@gmail.com>
2022-07-20 20:12:18 +0300

    continue config

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # webpack
 setting up and project on webpack
+
+# project build (scripts in package.json)
+"npm run dev" - development mode
+"npm run build" - production mode
+"npm run watch" - development mode with watch the files
+
+can import json file in js file
+
+# use plugins
+1) html-webpack-plugin
+2) clean-webpack-plugin
+
+# use loaders
+1) css-loader
+2) style-loader
+3) file-loader (for img best to use (type: 'asset/resource') )
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,35 @@
-// import { webpack } from "webpack";
-
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+    context: path.resolve(__dirname, 'src'), // Webpack будет отталкиватся от этой папки. В entry можно прописывать путь без указания этой папки
     mode: 'development', // мод разработка или продакшн
     entry: {
-        main: './src/index.js',
-        analytics: './src/analytics.js'
+        main: './index.js',
+        analytics: './analytics.js'
     }, // входноной (начальный) файл
     output: { // куда складывать и это обьект
         filename: '[name].[contenthash].js', // Имя по умолчанию пишут bundle. Можно добавить паттерн имени [name], [contanthash]
         path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [ // имеют сущность классов
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            inject: 'body'
+        }),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/, // если webpack встречает такие файлы то далее идут типы лоадеров
+                use: ['style-loader', 'css-loader'] // выполняется справа на лево
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                type: 'asset/resource'
+            }
+        ]
     }
 };
